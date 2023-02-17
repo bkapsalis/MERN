af651030dc35240a940b8e4096d87f18b4e14791
Bill Kapsalis <bkapsalis@gmail.com>
2023-02-16 23:21:18 -0800

    added build

---
+++ client/build/precache-manifest.d8dcac5096284d7d17e85d11155f8546.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697e56afa07478135037c4668c74b2e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c26d3b2c2566972ae67d",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "d1bbe28377c64a67e033",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "c26d3b2c2566972ae67d",
+    "url": "/static/js/2.1991f109.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.1991f109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bbe28377c64a67e033",
+    "url": "/static/js/main.cc65eef4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.cc65eef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
